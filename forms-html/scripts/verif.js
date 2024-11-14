b6dee14079717935785f38341f67c4c9f4067317
Carcamano5 <gmazzega2@gmail.com>
2024-11-14 16:33:49 -0300

    descrição do commit

--- forms-html/scripts/verif.js
+++ forms-html/scripts/verif.js
@@ -1,22 +1,99 @@
 function validar()
 {
-    event.preventDefault();
+    event.preventDefault()
+  let nome = document.getElementById('nome').value
+  let email = document.getElementById('email').value
+  let tell = document.getElementById('telefone').value
+  let data = document.getElementById('data').value
+  let correto = true;
+  
+  //valida nome
+  if(nome.trim() == '' || nome.length < 10)
+  {
+    let mensagem = document.getElementById('erro-nome')
+    mensagem.style.display = 'Flex'
+    mensagem.innerText = 'Seu nome está vazio ou possui menos que 10 caracteres'
+    mensagem.style.color = 'red'
+    correto = false
+  }
+  else
+  {
+    let mensagem = document.getElementById('erro-nome')
+    mensagem.style.display = 'none'
+  }
 
-   const email = document.getElementById('email').value
-   const senha = document.getElementById('senha').value
+  //valida email
+  if(email == ' ' || !email.includes('@') || !email.includes('.'))
+  {
+    let mensagem = document.getElementById('erro-email')
+    mensagem.style.display = 'Flex'
+    mensagem.innerText = 'Seu email está vazio, ou deve possuir um @ e um .'
+    mensagem.style.color = 'red'
+    correto = false
+  }
+  else
+  {
+    let mensagem = document.getElementById('erro-email')
+    mensagem.style.display = 'none'
+  }
 
-   if(!email.include('@'))
-   {
-        alert('Insira um email valido!')
-        return false
-   }
+  //valida telefone
+  var regex = /^(?:\+)[0-9]{2}\s?(?:\()[0-9]{2}(?:\))\s?[0-9]{4,5}(?:-)[0-9]{4}$/
+  if(!regex.test(tell))
+  {
+    let mensagem = document.getElementById('erro-telefone')
+    mensagem.style.display = 'Flex'
+    mensagem.innerText = 'Seu telefone deve estar como no exemplo, +55 (55) 23321-5454 '
+    mensagem.style.color = 'red'
+    correto = false
+  }
+  else
+  {
+    let mensagem = document.getElementById('erro-telefone')
+    mensagem.style.display = 'none'
+  }
 
-   if(senha.length < 6)
-   {
-        alert('Digite uma senha com pelo menos 6 caracteres')
-        return false
-   }
+  //valida data
+  if(!data)
+  {
+    let mensagem = document.getElementById('erro-data')
+    mensagem.style.display = 'Flex'
+    mensagem.innerText = 'Informe uma data de nascimento '
+    mensagem.style.color = 'red'
+    correto = false
+  }
+  else{
+      let dataSel = new Date(data)
+      let dataAtual = new Date()
 
-   window.location.href = "teacher-home.html"
-   return true
+      let idadeMin = 12
+      let idadeMax = 19
+
+      let dataMin = new Date();
+      dataMin.setFullYear(dataAtual.getFullYear() - idadeMax);
+      
+      let dataMax = new Date();
+      dataMax.setFullYear(dataAtual.getFullYear() - idadeMin)
+
+      let mensagem = document.getElementById('erro-data')
+      mensagem.style.display = 'none'
+      if(dataSel > dataMax || dataSel < dataMin)
+      {
+        let mensagem = document.getElementById('erro-data')
+        let mensagem2 = document.getElementById('erro-data2')
+        mensagem.style.display = 'Flex'
+        mensagem2.style.display = 'Flex'
+        mensagem.innerText = 'A data não pode estar no futuro'
+        mensagem2.innerText = `A data deve corresponder a uma idade entre ${idadeMin} e ${idadeMax} anos.`
+        mensagem.style.color = 'red'
+        mensagem2.style.color = 'red'
+        correto = false
+      }
+  }
+
+  //deu tudo certo? exibir a tabela com os alunos cadastrados!
+  if(correto == true)
+  {
+    alert('teste')
+  }
 }
